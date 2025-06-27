35c09baf4907361d87b8e066ae38c1964dbf557f
NoroHarry <mouadhari250@gmail.com>
2025-06-27 20:01:40 +0100

    message

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',            // activation de l'export statique automatique
-  basePath: '/The-Key-demo', // <-- nom exact de ton dossier ou repo GitHub
+  basePath: '/', // <-- nom exact de ton dossier ou repo GitHub
   images: {
     unoptimized: true,
   },
