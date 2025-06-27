2d8f78b721fff335f4983b13c977e65e19df9562
NoroHarry <mouadhari250@gmail.com>
2025-06-27 19:43:39 +0100

    message

--- the-key-landing/next.config.mjs
+++ the-key-landing/next.config.mjs
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',            // activation de l'export statique automatique
+  basePath: '/the-key-landing', // <-- nom exact de ton dossier ou repo GitHub
+  images: {
+    unoptimized: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
   },
 }
 
