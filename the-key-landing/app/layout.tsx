9220789d54a9bdb7812ea9b9fd9eee589686bc08
NoroHarry <mouadhari250@gmail.com>
2025-06-27 19:48:29 +0100

    message

--- the-key-landing/app/layout.tsx
+++ the-key-landing/app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'The Key',
+  keywords: ['landing page', 'next.js', 'react', 'tailwindcss'],
+  description: 'The Key',
 }
 
 export default function RootLayout({
